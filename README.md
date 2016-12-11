7cc4b3945708dadcb1bd1ea0970a1e700a53b378
陆仁杰 <tracy.lu@ximalaya.com>
2016-12-11 16:31:08 +0800

    最佳W [-462, -41, -17, -34, -19, -115, -10, -16, 1, -49, -49, -520, -13, 11, 1, 1] 最佳准确率 0.877903118779

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 3 - marital : marital status (categorical: 'divorced','married','single','unknown'; note: 'divorced' means divorced or widowed)
 
-4 - education (categorical: 'basic.4y','basic.6y','basic.9y','high.school','illiterate','professional.course','university.degree','unknown')
+4 - education (categorical: 'basic.4y','basic.6y','basic.9y','high.school','primary','illiterate','professional.course','university.degree','unknown')
 
 5 - default: has credit in default? (categorical: 'no','yes','unknown')
 
--- plaAlgorithm.py
+++ plaAlgorithm.py
@@ -1,16 +1,56 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import csv
 
 # 迭代次数
-ITERATION = 70
+ITERATION = 110
 # 初始化向量
-W = [1, 1, 1]
+W = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+need_transfer_list = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome']
+# 存放需要map字符串与分数的map
+score_map = {}
+
+
+# 将原始数据经过变换以后持久化存储到txt文件中
+def processOriginalData2txt():
+    test_set = csv.reader(open('data/bank.csv', 'rb'))
+    train_set = csv.reader(open('data/bank-full.csv', 'rb'))
+    return saveOriginalData2txt(train_set, 'train.txt'), saveOriginalData2txt(test_set, 'test.txt')
+
+
+def saveOriginalData2txt(lines, file_name):
+    dataList = []
+    for line in lines:
+        dataLine = line[0].strip().replace('"', '').replace("'", '').split(';')
+        dataList.append(dataLine)
+    names = dataList.pop(0)
+    if not score_map:
+        build_data2map(names, dataList)
+    result_list = []
+    for i in range(len(dataList)):
+        sub_numbers = []
+        for j in range(len(dataList[i]) - 1):
+            number = dataList[i][j]
+            if score_map.__contains__(names[j]):
+                number = score_map.get(names[j]).get(dataList[i][j])
+            sub_numbers.append(int(number))
+        if 'no' == dataList[i][-1]:
+            sub_numbers.append(-1)
+        else:
+            sub_numbers.append(1)
+        result_list.append(sub_numbers)
+    fileObject = open(file_name, 'w')
+    for ip in result_list:
+        for item in ip:
+            fileObject.write(str(item))
+            fileObject.write(' ')
+        fileObject.write('\n')
+    fileObject.close()
 
 
 def createDate():
-    lines_set = open('../data/Dataset_PLA.txt').readlines()
-    train_set = lines_set[1:7]
-    test_set = lines_set[9:13]
+    train_set = open('train.txt').readlines()
+    test_set = open('test.txt').readlines()
     return processData(train_set), processData(test_set)
 
 
@@ -24,12 +64,39 @@ def processData(lines):
     return dataList
 
 
+# 将需要将name解释成分数的list构造成score_map
+def build_data2map(names, data_list):
+    all_item_dic = {}
+    fileObject = open('name_score.txt', 'w')
+    for i in range(len(data_list)):
+        for j in range(len(data_list[i])):
+            if all_item_dic.__contains__(names[j]):
+                item_list = all_item_dic[names[j]]
+            else:
+                item_list = set([])
+            item_list.add(data_list[i][j])
+            all_item_dic[names[j]] = item_list
+    for name in all_item_dic.keys():
+        if need_transfer_list.__contains__(name):
+            item_score = {}
+            item_list = list(all_item_dic.get(name))
+            fileObject.write(name + ':')
+            for i in range(len(item_list)):
+                if 'unknown' == item_list[i]:
+                    item_score[item_list[i]] = 0
+                else:
+                    item_score[item_list[i]] = i + 1
+            fileObject.write(str(item_score))
+            fileObject.write('\n')
+            score_map[name] = item_score
+    fileObject.close()
+
+
 # 将一个向量与权重向量相乘,如果值大于0则返回1,反之返回-1
 def sign(W, dataList):
     sum = 0
     for i in range(len(W)):
         sum += W[i] * dataList[i]
-
     if sum > 0:
         return 1
     return -1
@@ -48,14 +115,24 @@ def reSetW(W, trainData):
 def train(W, trainDatas):
     for i in range(ITERATION):
         index = i % len(trainDatas)
-        W = reSetW(W, trainDatas[i])
+        W = reSetW(W, trainDatas[index])
+    return W
 
-def predictData(W,train_set,test_set):
-    newW=train(W,train_set)
+
+# 使用测试集去测试训练后的W的正确度
+def predictData(W, train_set, test_set):
+    newW = train(W, train_set)
     print newW
+    error_count = 0
     for i in range(len(test_set)):
-        result=sign(newW,test_set[i])
-        print result
+        result = sign(newW, test_set[i])
+        if result != int(test_set[i][-1]):
+            error_count += 1
+    print 1.0 * (len(test_set) - error_count) / len(test_set)
 
-trainData,testData=createDate()
-predictData(W,trainData,testData)
+
+##避免多次format,只有第一次执行,以后只关注训练
+# processOriginalData2txt()
+
+trainData, testData = createDate()
+predictData(W, trainData, testData)
